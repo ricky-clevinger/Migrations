6eaba76924da305e908bef28faa4ea1e5cacaba8
ricky-clevinger <ricky.clevinger@cgi.com>
2017-09-07 16:02:06 -0400

    New Script

--- Test/V0__Baseline.sql
+++ Test/V0__Baseline.sql
@@ -1,3 +1,9 @@
+--------------------------------------------------------
+--  File created - Thursday-September-07-2017   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table Book
+--------------------------------------------------------
 
   CREATE TABLE "ORACLE"."Book" 
    (	"ID" VARCHAR2(20 BYTE), 
@@ -17,3 +23,20 @@
   PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
  NOCOMPRESS LOGGING
   TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  DDL for Table User
+--------------------------------------------------------
+
+  CREATE TABLE "ORACLE"."User" 
+   (	"ID" VARCHAR2(20 BYTE), 
+	"Name" VARCHAR2(20 BYTE)
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+REM INSERTING into ORACLE."Book"
+SET DEFINE OFF;
+REM INSERTING into ORACLE."Member"
+SET DEFINE OFF;
+REM INSERTING into ORACLE."User"
+SET DEFINE OFF;
